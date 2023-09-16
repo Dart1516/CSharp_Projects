4be6e622739e48cfee14606f7653c231104551b8
dart1516 <danvddpf1@gmail.com>
2023-09-16 18:08:19 -0300

    a new of on ADVICE

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -6,5 +6,47 @@ class Program
     {
         Console.WriteLine("Hello Sandbox World!");
         Console.WriteLine("I'm changing some stuff here because I want to learn !");
+        Console.WriteLine("");
+        Console.WriteLine("Practicing GIT");
+        Console.WriteLine("");
+        Console.WriteLine("STATUS OF A GIT");
+        Console.WriteLine("to see the status of our git: ");
+        Console.WriteLine("git status");
+        Console.WriteLine("");
+        Console.WriteLine("");
+        Console.WriteLine("ADD TO A GIT");
+        Console.WriteLine(" To add a new change on our git, we should write GIT ADD and then the address of the files, example:");
+        Console.WriteLine("git add sandbox/Sandbox/Program.cs");
+        Console.WriteLine("");
+        Console.WriteLine("");
+        Console.WriteLine("RESTORE A GIT ");
+        Console.WriteLine("restore a git is mean to forget the change that we add on the git and it will be:");
+        Console.WriteLine("git restore");
+        Console.WriteLine("");
+        Console.WriteLine("");
+        Console.WriteLine("ADD to GIT Faster");
+        Console.WriteLine("we can use a dot (.) to be faster in adding a git, instead of writing all the address, example: ");
+        Console.WriteLine("git add .");
+        Console.WriteLine("");
+        Console.WriteLine("GIT COMMIT");
+        Console.WriteLine("if we only write git commit it will give an error and in order to scape we should write:");
+        Console.WriteLine(":q!");
+        Console.WriteLine("soi to commit something we should write:");
+        Console.WriteLine("git commit -m (follow by phrase)");
+        Console.WriteLine("");
+        Console.WriteLine("GIT PUSH");
+        Console.WriteLine("when we want to save the change on github we should use:");
+        Console.WriteLine("git push");
+        Console.WriteLine("");
+        Console.WriteLine("ADVICE");
+        Console.WriteLine("eveything can be done on a single page, example");
+        Console.WriteLine("git add.");
+        Console.WriteLine("git commit-m 'one last line' ");
+        Console.WriteLine("");
+        Console.WriteLine("");
+        Console.WriteLine("");
+
+        
+        
     }
 }
