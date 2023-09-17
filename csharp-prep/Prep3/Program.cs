c73848a99da7127c6509ffad4dcd6d2526a22ae5
dart1516 <danvddpf1@gmail.com>
2023-09-16 21:01:17 -0300

    activity 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
-    }
+        //Console.WriteLine("What is the magic number? ");
+        //string answer1 = Console.ReadLine();
+        //int secret_number = int.Parse(answer1);
+        
+        Console.WriteLine("Wellcome to this game where do you have to guess a number between 1 - 100");
+        Console.WriteLine("we wish you good luck");
+        Console.WriteLine("");
+
+        string  game_on ;
+        do {
+        
+        Random randomGenerator = new Random();
+        int secret_number = randomGenerator.Next(1, 100);
+        int my_guest = 0;
+
+        int how_many_tries = 0; 
+        while (my_guest != secret_number) {
+        Console.Write("What is your guess? ");
+        string answer2 = Console.ReadLine();
+        my_guest = int.Parse(answer2);
+
+            if (my_guest > secret_number )
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (my_guest < secret_number){
+                Console.WriteLine("lower");
+            }
+            how_many_tries ++;
+        }
+        Console.WriteLine("You guessed it!");
+        Console.WriteLine($"and you only try it {how_many_tries} times");
+        Console.Write("Do you want to play again? ");
+        game_on = Console.ReadLine();
+}while (game_on == "yes");{
+}  
+
+}
 }
