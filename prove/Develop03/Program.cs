91ec17a2c6ca1a4156f5344a99a3094d4309f98c
dart1516 <danvddpf1@gmail.com>
2023-09-30 02:04:20 -0300

    delete the other Program File

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,40 @@
-using System;
+﻿using System;
+using System.Diagnostics;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Scripture s1 = new Scripture();
+        string reference = s1.getReference();
+        string originalScriptureText = s1.getScriptureText();
+        Word w1 = new Word(originalScriptureText);
+        string modifiedText = w1.getModifiedText();
+        Console.WriteLine("Well come to the Scripture Mastery Passages. ");
+        Console.WriteLine($"Today we will memorize this scripture: \n{reference}");
+        Console.WriteLine($"{originalScriptureText}");
+        Console.WriteLine("");
+        string IsCompletelyHidden = s1.getIsCompletelyHidden();
+        do
+        {   
+            Console.WriteLine("Press Enter twice to hide some words or write 'quit' to finish");
+            string input = Console.ReadLine()!;
+            ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+
+            if (keyInfo.Key == ConsoleKey.Q)
+            {
+                break;
+            }
+            if(input.ToLower() == "quit")
+            {
+                break;
+            }
+            
+            s1.HideRandomWords2();
+            s1.GetDisplayText();
+            Console.WriteLine($"");
+        }while (IsCompletelyHidden == "no");
+
+        Console.WriteLine("Thank you, see you next time!");
     }
 }
